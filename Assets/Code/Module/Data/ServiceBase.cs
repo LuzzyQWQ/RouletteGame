811a56bbee6784dde74b8db32c3d74150d35da87
Luzzy <luzzyzerio@gmail.com>
2024-03-10 17:18:48 +0800

    添加ES3数据保存工具

---
+++ Assets/Code/Module/Data/ServiceBase.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Argali.Module.DataBase
+{
+	/// <summary>
+	/// 基于ES3的数据服务抽象类
+	/// </summary>
+
+	public abstract class ServiceBase
+    {
+        protected readonly ES3File _dataFile;
+        private readonly Dictionary<string, object> _cache;
+
+        protected ServiceBase()
+        {
+            string path = $"bb2_es3/{GetType().Name}";
+            var es3Setting = new ES3Settings{ path = path };
+            _dataFile = new ES3File(es3Setting, true);
+            _cache = new Dictionary<string, object>();
+        }
+
+        public virtual void Sync()
+        {
+            _dataFile.Sync();
+        }
+
+        public virtual void Clear()
+        {
+            if (_dataFile != null)
+            {
+                _dataFile.Clear();
+                _cache.Clear();
+                Sync();
+            }
+        }
+
+        protected T LoadWithCache<T>(string key)
+        {
+            if (!_cache.ContainsKey(key))
+            {
+                _cache.Add(key, _dataFile.Load(key));
+            }
+
+            return (T)_cache[key];
+        }
+
+        protected T LoadWithCache<T>(string key, T defaultValue)
+        {
+            if (!_cache.ContainsKey(key))
+            {
+                _cache.Add(key, _dataFile.Load(key, defaultValue));
+            }
+
+            return (T)_cache[key];
+        }
+
+        protected void SaveWithCache<T>(string key, T value)
+        {
+            _cache[key] = value;
+            _dataFile.Save(key, value);
+        }
+    }
+}
---
+++ Assets/Code/Module/Data/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using Argali.Module.Singleton;
+using System;
+using System.Collections.Generic;
+
+namespace Argali.Module.DataBase
+{
+	/// <summary>
+	/// Service用于保存各种数据
+	/// </summary>
+    public class ServiceManager: Singleton<ServiceManager>
+    {
+        private static readonly List<ServiceBase> ServiceList = new();
+
+        public static T GetService<T>() where T: ServiceBase
+        {
+            ServiceBase service = null;
+            foreach (ServiceBase s in ServiceList)
+            {
+                if (s is T)
+                {
+                    service = s;
+                }
+            }
+
+            if (service == null)
+            {
+                var newService = (T) Activator.CreateInstance(typeof(T));
+                ServiceList.Add(newService);
+                return newService;
+            }
+            else
+            {
+                return (T)service;
+            }
+        }
+
+        public static void Sync()
+        {
+            foreach (ServiceBase service in ServiceList)
+            {
+                service.Sync();
+            }
+        }
+    }
+}
+

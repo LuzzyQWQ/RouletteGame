a91240438dbd3681a66f2d1d261cb6132cb6c0c3
Yang <yang@battlecryhq.com>
2024-03-28 10:38:18 +0800

    添加游戏场景 Controller

---
+++ Assets/Code/GamePlay/GameScene/Controller/RouletteGameSceneController.cs
@@ -0,0 +1,48 @@
+﻿using Argali.Game.RouletteSystem;
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Argali.Game.GameScene
+{
+	/// <summary>
+	/// 游戏场景 - 轮盘控制器
+	/// </summary>
+	public class RouletteGameSceneController
+	{
+		#region 
+		/// <summary>
+		/// 轮盘对象
+		/// </summary>
+		public RouletteObject RouletteObject;
+		#endregion
+
+		#region 构造
+		private RouletteGameSceneController()
+		{
+			RouletteObject = null;
+		}
+		/// <summary>
+		/// 创建一个新的轮盘游戏场景控制器
+		/// </summary>
+		/// <returns></returns>
+		public static async UniTask Create()
+		{
+			var instance = new RouletteGameSceneController();
+			var roulettePrefab = await Addressables.LoadAssetAsync<GameObject>("RouletteObject");
+			instance.RouletteObject = GameObject.Instantiate(roulettePrefab).GetComponent<RouletteObject>();
+			await instance.RouletteObject.CreateSlotObjects(RouletteSystemController.Instance.Roulette);
+		}
+		#endregion 
+
+
+		public void Destroy()
+		{
+			GameObject.Destroy(RouletteObject.gameObject);
+			RouletteObject = null;
+		}
+	}
+
+}
